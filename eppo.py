740b7109d1a5b9c3e6267fb134ff3f617f6cea08
LH2OQ <pina.qoohlada@pm.me>
2025-11-26 22:30:02 +0100

    Update eppo.py
    main module shall be conserved as that, so I outsourced the first steps to other files to be able to drop them if not needed anymore

--- eppo.py
+++ eppo.py
@@ -1,33 +1,14 @@
 from helper.html import HTML
-from helper.files import PDF, TEXT
+from helper.files import PATH, PDF, TEXT
+from eppo_first_aid import getFirstAid
 import os
 
-os.system("clear")
 url = "https://gd.eppo.int"
 
-def main():
-	html = HTML.new(url+"/taxon")
-	print(filterLinks(html), "\n")
-	print("\n\n".join([str(u) for u in getUserInstructions()]), "\n")
-	print(apiRequest("BANANA", "xyz"))
-		
-def filterLinks(html):
-	data = ""
-	for l in HTML.links(html):
-		href = l["href"]
-		text = HTML.text(l)
-		if "PP" in text or "PM" in text:
-			data = "\n".join([data, text, href])
-	return data
-	
-def getUserInstructions():
-	text = []
-	pdf = PDF.link("https://gd.eppo.int/media/files/general_user-guide.pdf")
-	for page in pdf.pages:
-		t = TEXT.readline(page.extract_text())
-		text.append(t)
-	return(text)
 
+def main():
+	getFirstAid()
+	
 def apiRequest(eppoCode, authToken):
 	return HTML.get(
 		f"https://data.eppo.int/api/rest/1.0/taxon/{eppoCode}?",
