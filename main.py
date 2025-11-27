f6a5a290f3b4967c2f1f6a01824d90fddb797398
LH2OQ <pina.qoohlada@pm.me>
2025-11-27 12:44:55 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from helper.html import HTML
 from helper.files import PATH, PDF, TEXT
-from eppo_first_aid import getFirstAid
+from helper.eppo_first_aid import getFirstAid
 
 def main():
+	# prompt the llm for help
 	getFirstAid()
 	
 def apiRequest(eppoCode, authToken):
+	# make an api-request to www.eppo.int to find information by specific token (eppoCode)
 	return HTML.get(
 		f"https://data.eppo.int/api/rest/1.0/taxon/{eppoCode}?",
 		{"authtoken": authToken}
