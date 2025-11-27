33261259fd6045cd6fe1696e658138defeffeae8
LH2OQ <pina.qoohlada@pm.me>
2025-11-27 12:41:41 +0100

    Update and rename llm.py to helper/llm.py

--- helper/llm.py
+++ helper/llm.py
@@ -6,12 +6,18 @@ api_key = "BITTE_HIER_api_key_EINFÜGEN"
 
 
 DEFAULT_SYSTEM_PROMPT = """
-		Du bist ein hilfreicher Assistent - deine Aufgabe ist es, Anfragen so klar und verständlich wie möglich zu beantworten.
-		Dabei ist es wichtig, einfache und kurze Sätze zu wählen, auf komplizierte Fachwörter zu verzichten, bzw. diese zu beschreiben oder Synonyme zu benutzen, und die zur Anfrage relevante Information so auszugeben, dass eine jugendliche Person mit dem Bildungshintergrund einer deutschen Hauptschule diese nachvollziehen, interpretieren und lösungsorientiert anwenden kann.
+		Du bist ein hilfreicher Assistent.
+		Deine Aufgabe ist es, Anfragen so klar und verständlich wie möglich zu beantworten.
+		Dabei ist es wichtig, einfache und kurze Sätze zu wählen,
+		auf komplizierte Fachwörter zu verzichten, bzw. diese zu beschreiben oder Synonyme zu benutzen,
+		Diese zur Anfrage relevante Information soll so ausgegeben werden,
+		dass eine Person mit dem Bildungshintergrund einer deutschen Hauptschule diese nachvollziehen,
+		interpretieren und lösungsorientiert anwenden kann.
 	"""
 MAX_TOKENS = 150
 
 def getClient():
+	# openai client request
 	client = OpenAI(
 		base_url = base_url,
 		api_key = api_key
@@ -20,6 +26,7 @@ def getClient():
 	return client
 
 def askLLM(client, prompt: str, system_prompt: str = DEFAULT_SYSTEM_PROMPT):
+	# prompt the llm and get a response
 	response = client.chat.completions.create(
 		model = "LLama-4-Scout",
 		messages = [{
@@ -32,15 +39,12 @@ def askLLM(client, prompt: str, system_prompt: str = DEFAULT_SYSTEM_PROMPT):
 		}],
 		max_tokens = MAX_TOKENS
 	).choices[0].message.content
-	
-	# Ich halte es für sinnvoll, den Dialog in Textdateien zu speichern,
-	# da so schon einmal hilfreich beantworte Anfrag nicht erneut gestellt werden müssen.
-	# Das spart Strom, etc ...
+	# save the conversation as a textfile in a subdirectory => current_directory/LLM
 	folder = PATH.mkdir(PATH.cwd() + "/LLM")
+	# n is the number of files that already exist in that directory (in case of multiple saves, 0 if empty)
 	n = len([name for name in os.listdir(folder) if os.path.isfile(name)])
+	# format prompt and response (+ max. number of tokens) as text, then write to file 
 	text = f"PROMPT:\n\n{prompt}\n\nRESPONSE({MAX_TOKENS}):\n\n{response}"
 	PATH.write(folder + f"/text_{n}", text)
 	print(response)
 	return response
-
-
